469e326789196a398dc1c9216fc55640d494aaf6
Ebanga Loic <loic.ebanga@orange.com>
2024-12-26 17:01:05 +0100

    Gestions des erreurs

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -5,6 +5,8 @@ import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 export const router = createBrowserRouter([
     {
@@ -15,7 +17,10 @@ export const router = createBrowserRouter([
             { path: 'catalog', element: <Catalog /> },
             { path: 'catalog/:id', element: <ProductDetails /> },
             { path: 'about', element: <AboutPage /> },
-            { path: 'contact', element: <ContactPage /> }
+            { path: 'contact', element: <ContactPage /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: '*', element: <NotFound /> },
         ]
     }
 ])
